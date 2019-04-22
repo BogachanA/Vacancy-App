ef328a0df8f7a19b38196fa6d5b19eec5bbed515
boğaçhan arslan <bogacbugra@gmail.com>
2019-04-23 00:58:25 +0300

    Done with models and manytomany relations. Adjusted admin panel views.

--- classAvailable/admin.py
+++ classAvailable/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('name','capacity','exam_capacity')
+    ordering = ('name',)
+
+class ResAdmin(admin.ModelAdmin):
+    list_display = ('by','get_class_list','instructor','res_date_start','res_date_end')
+    ordering = ('res_date_start','by')
+
+
+admin.site.register(Classroom,ClassAdmin)
+admin.site.register(Reservation,ResAdmin)
--- classAvailable/models.py
+++ classAvailable/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import datetime
+
 
 # Create your models here.
+class Classroom(models.Model):
+    name = models.CharField(max_length=15, null=False, default="001")
+    capacity = models.IntegerField(default=0)
+    exam_capacity = models.IntegerField(default=0)
+    # Reservations
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    by = models.ForeignKey(get_user_model(), null=False, default=1, on_delete=models.SET_DEFAULT) #TODO user
+    res_class = models.ManyToManyField(Classroom)
+    instructor = models.CharField(max_length=100, null=True)
+    proctor = models.CharField(max_length=100, null=True)
+    res_date_start = models.DateTimeField(null=True)
+    res_date_end = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return "Reservation for {0} by ({1}) on {2}".format(self.get_class_list(),self.instructor,self.res_date_start)
+
+    def get_class_list(self):
+        return ", ".join([c.name for c in self.res_class.all()])
+
+
